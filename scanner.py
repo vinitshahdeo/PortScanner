1b176225950ba38a9b7de18b93c3655cf622f377
Kashish121 <56366103+Kashish121@users.noreply.github.com>
2020-06-11 11:21:35 +0530

    seperate module for multithreading

--- scanner.py
+++ scanner.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import json
 import threading
 import __builtin__
+from scanner_thread import split_processing
 
 exc = getattr(__builtin__, "IOError", "FileNotFoundError")
 
@@ -30,6 +31,8 @@ try:
         config = json.load(config_file)
     range_high = int(config['range']['high'])
     range_low = int(config['range']['low'])
+    # defining number of threads running concurrently
+    CONST_NUM_THREADS = int(config['thread']['count'])
 
 except IOError:
     print("config.json file not found")
@@ -37,7 +40,7 @@ except ValueError:
     print("Kindly check the json file for appropriateness of range")
 
 ports = list(range(range_low, range_high, 1))
-# scanning the port only in range of (1, 8888)
+# scanning the port only in range of (range_low, range_high)
 
 
 def scan(ports, range_low, range_high):
@@ -62,29 +65,8 @@ def scan(ports, range_low, range_high):
         sys.exit()
 
 
-# defining number of threads running concurrently
-CONST_NUM_THREADS = 8
-
-
-def split_processing(ports, num_splits=CONST_NUM_THREADS):
-    split_size = (range_high-range_low) // num_splits
-    threads = []
-    for i in range(num_splits):
-        # determine the indices of the list this thread will handle
-        start = i * split_size
-        # special case on the last chunk to account for uneven splits
-        end = range_high if i+1 == num_splits else (i+1) * split_size
-        # create the thread
-        threads.append(
-            threading.Thread(target=scan, args=(ports, start, end)))
-        threads[-1].start()  # start the thread we just created
-
-    # wait for all threads to finish
-    for t in threads:
-        t.join()
-
-
-split_processing(ports)
+# calling function from scanner_thread.py for multithreading
+split_processing(ports, CONST_NUM_THREADS, scan, range_low, range_high)
 
 # Checking the time again
 t2 = datetime.now()
