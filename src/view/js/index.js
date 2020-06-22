e3d41e607a5509ac82bbb03486a82d498e42ee51
ishika1727 <ishikadubey2000@gmail.com>
2020-06-23 01:40:27 +0530

    camelCase used in index.js

--- src/view/js/index.js
+++ src/view/js/index.js
@@ -1,38 +1,36 @@
 $(document).ready(function () {
     $("#history").show();
     $("#timer").hide();
-
-
 });
 
-var data = 0;
-function scan_port() {
+var data, minutesLabel, secondsLabel, totalSeconds, valString;
+data = 0;
+
+function scanPort() {
+
     if (data == 0) {
         data = 1;
         $("#history").hide();
         $("#timer").show();
-
         setInterval(setTime, 1000);
+        minutesLabel = document.getElementById("minutes");
+        secondsLabel = document.getElementById("seconds");
+        totalSeconds = 0;
+    }  
+}
 
-        var minutesLabel = document.getElementById("minutes");
-        var secondsLabel = document.getElementById("seconds");
-        var totalSeconds = 0;
+function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+    valString = val + "";
+    if (valString.length < 2) {
+        return "0" + valString;
     }
-
-    function setTime() {
-        ++totalSeconds;
-        secondsLabel.innerHTML = pad(totalSeconds % 60);
-        minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+    else {
+        return valString;
     }
-
-    function pad(val) {
-        var valString = val + "";
-        if (valString.length < 2) {
-            return "0" + valString;
-        }
-        else {
-            return valString;
-        }
-    }
-
 }
