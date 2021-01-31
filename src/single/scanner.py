c5fc1de0a56034d3f03d234a1afb51b4fcc991af
Vinamrata1086 <59366156+Vinamrata1086@users.noreply.github.com>
2021-01-31 16:41:35 +0530

    resolved the issue

--- src/single/scanner.py
+++ src/single/scanner.py
@@ -13,26 +13,20 @@ subprocess.call('cls', shell=True)
 remoteServer = input("Enter a remote host to scan: ")
 remoteServerIP = socket.gethostbyname(remoteServer)
 
-
 # Print a nice banner with information on which host we are about to scan
-print("-" * 60)
-print("Please wait, scanning remote host....", remoteServerIP)
-print("-" * 60)
-
+print ("-" * 60)
+print ("Please wait, scanning remote host....", remoteServerIP)
+print ("-" * 60)
 
 # Adding config.json path
 def get_absolute_path(relative_path):
     dir = os.path.dirname(os.path.abspath(__file__))
-    new = os.path.dirname(os.path.dirname(dir))
-    print(new)
     split_path = relative_path.split("/")
     absolute_path = os.path.join(new, *split_path)
     return absolute_path
 
-
 # Check what time the scan started
 t1 = datetime.now()
-
 
 # Pull port range from config.json
 with open(get_absolute_path('/config.json')) as config_file:
@@ -40,33 +34,32 @@ with open(get_absolute_path('/config.json')) as config_file:
     loRange = int(config['range']['low'])
     hiRange = int(config['range']['high'])
 
-
 # scanning the port in range specified in config.json
 try:
-    for port in range(loRange, hiRange):
+    for port in range(loRange,hiRange):
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((remoteServerIP, port))
         if result == 0:
-            print("Port {}: 	 Open".format(port))
+            print ("Port {}: 	 Open".format(port))
         sock.close()
 
 except KeyboardInterrupt:
-    print("You pressed Ctrl+C")
+    print ("You pressed Ctrl+C")
     sys.exit()
 
 except socket.gaierror:
-    print("Hostname could not be resolved. Exiting")
+    print ("Hostname could not be resolved. Exiting")
     sys.exit()
 
 except socket.error:
-    print("Couldn't connect to server")
+    print ("Couldn't connect to server")
     sys.exit()
 
 # Checking the time again
 t2 = datetime.now()
 
 # Calculates the difference of time, to see how long it took to run the script
-total = t2 - t1
+total =  t2 - t1
 
 # Printing the information to screen
-print('Scanning Completed in: ', total)
+print ('Scanning Completed in: ', total)
